b040be56e6d77d5ae8114968848c764b7b0a4e49
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2026-05-20 11:13:18 +0200

    feat: add license

--- src/gtfs-loader.js
+++ src/gtfs-loader.js
@@ -2,6 +2,23 @@
  * GTFS data loader for a web worker context (uses fetch).
  * Parses the VTER v1 binary timetable format produced by scripts/build_timetable.rb.
  * @module gtfs-loader
+ *
+ * ╭────────────────────────────────────────────────────────────────────────╮
+ * │ Copyright (C) 2026-present  Ulysse Buonomo                             │
+ * │                                                                        │
+ * │ This program is free software: you can redistribute it and/or modify   │
+ * │ it under the terms of the GNU General Public License as published by   │
+ * │ the Free Software Foundation, either version 3 of the License, or      │
+ * │  (at your option) any later version.                                   │
+ * │                                                                        │
+ * │ This program is distributed in the hope that it will be useful,        │
+ * │ but WITHOUT ANY WARRANTY; without even the implied warranty of         │
+ * │ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          │
+ * │ GNU General Public License for more details.                           │
+ * │                                                                        │
+ * │ You should have received a copy of the GNU General Public License      │
+ * │ along with this program.  If not, see <https://www.gnu.org/licenses/>. │
+ * ╰────────────────────────────────────────────────────────────────────────╯
  */
 
 // ---------- internal helpers ----------
--- src/parameters.js
+++ src/parameters.js
@@ -1,6 +1,23 @@
 /**
  * Global parameters for the voyage-ter project.
  *
+ * ╭────────────────────────────────────────────────────────────────────────╮
+ * │ Copyright (C) 2026-present  Ulysse Buonomo                             │
+ * │                                                                        │
+ * │ This program is free software: you can redistribute it and/or modify   │
+ * │ it under the terms of the GNU General Public License as published by   │
+ * │ the Free Software Foundation, either version 3 of the License, or      │
+ * │  (at your option) any later version.                                   │
+ * │                                                                        │
+ * │ This program is distributed in the hope that it will be useful,        │
+ * │ but WITHOUT ANY WARRANTY; without even the implied warranty of         │
+ * │ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          │
+ * │ GNU General Public License for more details.                           │
+ * │                                                                        │
+ * │ You should have received a copy of the GNU General Public License      │
+ * │ along with this program.  If not, see <https://www.gnu.org/licenses/>. │
+ * ╰────────────────────────────────────────────────────────────────────────╯
+ *
  * MIN_CONNECTION_TIME_SECONDS:
  *   Minimum required connection time between trains, expressed in seconds.
  *   The Connection Scan Algorithm (csa.js) in this project uses Unix timestamps
